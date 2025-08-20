c2c8453e828fc8bcec5f363fb7d111b539622d64
kanagavalli92 <kanaga430@gmail.com>
2025-08-20 16:55:20 +0800

    (feat) Add preliminary test cases

---
+++ src/test/java/first/webapp/RegisterServletTest.java
@@ -0,0 +1,13 @@
+package first.webapp;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class RegisterServletTest {
+	@Test
+	void firstTest() {
+	assertEquals (5, 5);
+}
+
+
+}
